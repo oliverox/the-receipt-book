fdab4635d617482f989711e5d0ef3f420523f7a1
Oliver Oxenham <oliver.oxenham@gmail.com>
2025-05-08 23:25:42 +0400

    full width

--- components/dashboard-shell.tsx
+++ components/dashboard-shell.tsx
@@ -22,7 +22,7 @@ export function DashboardShell({ children }: DashboardShellProps) {
   return (
     <div className="flex min-h-screen flex-col">
       <SidebarProvider>
-        <div className="flex h-screen">
+        <div className="flex h-screen w-full">
           <DashboardSidebar>
             <SidebarHeader className="border-b px-4 py-2">
               <div className="flex items-center gap-3 h-8">
